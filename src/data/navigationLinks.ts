3d0afefdd239fe1e05fdeb4de906beffc47bb239
José Aular <jose14aular@gmail>
2023-01-17 10:38:11 -0400

    Add blog link

--- src/data/navigationLinks.ts
+++ src/data/navigationLinks.ts
@@ -1,5 +1,8 @@
 import type { NavigationLink } from "~/lib/types";
 
-const navigationLinks: NavigationLink[] = [{ name: "Inicio", href: "/" }];
+const navigationLinks: NavigationLink[] = [
+  { name: "Inicio", href: "/" },
+  { name: "Blog", href: "/blog" },
+];
 
 export default navigationLinks;
