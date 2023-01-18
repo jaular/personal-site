9ae1f6b8013ab8b62b601ad6ea2a676eab3f938c
José Aular <jose14aular@gmail>
2023-01-18 11:56:43 -0400

    Add twitter icon

--- src/data/socialLinks.ts
+++ src/data/socialLinks.ts
@@ -3,17 +3,22 @@ import type { SocialLink } from "~/lib/types";
 const socialLinks: SocialLink[] = [
   {
     href: "https://www.linkedin.com/in/jos%C3%A9-aular-804656171/",
-    title: "Go to LinkedIn",
+    title: "LinkedIn",
     name: "linkedin",
   },
   {
     href: "https://github.com/jaular",
-    title: "Go to Github",
+    title: "Github",
     name: "github",
   },
   {
+    href: "https://twitter.com/jaular14",
+    title: "Twitter",
+    name: "twitter",
+  },
+  {
     href: "https://discord.com/users/769656181865447464",
-    title: "Go to Discord",
+    title: "Discord",
     name: "discord",
   },
 ];
