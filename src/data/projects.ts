8879f48a0b6c3c252845ba5e45134a0195d8731c
Jose Aular <jose14aular@gmail.com>
2023-01-28 10:18:05 -0400

    Change imageUrl

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -4,22 +4,22 @@ const projects: Project[] = [
   {
     title: "Next.js (Preview mode)",
     href: "https://next-datocms-preview.vercel.app/",
-    imageUrl: `https://res.cloudinary.com/jaular/image/upload/v1660852286/Next.js_Preview_mode_vdecgn.jpg`,
+    imageUrl: `https://res.cloudinary.com/jaular/image/upload/v1674914652/projects/next-preview_o6ruhy.png`,
   },
   {
     title: "J&C Consulting Group C.A.",
     href: "https://jc-consgrp.vercel.app/",
-    imageUrl: `https://res.cloudinary.com/jaular/image/upload/v1660852286/J_C_Consulting_Group_C.A_te9ncs.jpg`,
+    imageUrl: `https://res.cloudinary.com/jaular/image/upload/v1674914652/projects/jc_bw0u9j.png`,
   },
   {
     title: "NT-Photos",
     href: "https://nt-photos.vercel.app/",
-    imageUrl: `https://res.cloudinary.com/jaular/image/upload/v1660852286/NT-Photos_yqmlsz.jpg`,
+    imageUrl: `https://res.cloudinary.com/jaular/image/upload/v1674914652/projects/nt-photos_cb52c5.png`,
   },
   {
     title: "HSL Colors",
     href: "https://hsl-colors.netlify.app/",
-    imageUrl: `https://res.cloudinary.com/jaular/image/upload/v1660852286/HSL_Colors_dhrlii.jpg`,
+    imageUrl: `https://res.cloudinary.com/jaular/image/upload/v1674914652/projects/colors_il3pof.png`,
   },
 ];
 
