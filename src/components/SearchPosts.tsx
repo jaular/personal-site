249facc4299cdeb35bde77b956c9c8e757c37a3e
Jose Aular <jose14aular@gmail.com>
2023-02-13 14:29:45 -0400

    Change dark mode color

--- src/components/SearchPosts.tsx
+++ src/components/SearchPosts.tsx
@@ -40,20 +40,18 @@ const SearchPosts: Component<Props> = ({ data }) => {
               setPosts(filteredBlogPosts);
             }}
             placeholder="Buscar publicaciones"
-            class="block w-full px-4 py-2 text-base text-black placeholder-gray-600 bg-gray-200/50 rounded dark:placeholder:text-gray-800 dark:text-white dark:bg-black-600 focus:outline focus:outline-1 focus:outline-blue"
+            class="block w-full px-4 py-2 text-base text-black placeholder-gray-600 bg-gray-200/50 rounded dark:placeholder:text-gray-800 dark:text-white dark:bg-black-700 focus:outline focus:outline-1 focus:outline-blue"
           />
           <svg
-            class="absolute w-5 h-5 text-gray-600 dark:text-gray-800 right-3 top-3"
+            class="absolute w-5 h-5 text-gray-600 dark:text-gray-800 right-3 top-2.5"
             xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
+            viewBox="0 0 20 20"
+            fill="currentColor"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width={2}
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              fill-rule="evenodd"
+              d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+              clip-rule="evenodd"
             />
           </svg>
         </div>
