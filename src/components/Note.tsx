10bb3c45a9926eda8a9704660eedff4b2a18d091
Jose Aular <jose14aular@gmail.com>
2023-02-16 11:58:01 -0400

    Remove publishDate

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,10 +1,8 @@
 import type { Component } from "solid-js";
-import { formattedDate } from "~/lib/utils";
 
 type NoteProps = {
   slug: string;
   title: string;
-  publishDate: Date;
 };
 
 type Props = {
@@ -12,7 +10,7 @@ type Props = {
 };
 
 const Note: Component<Props> = ({ note }) => {
-  const { slug, title, publishDate } = note;
+  const { slug, title } = note;
 
   return (
     <li class="relative py-4 first:pt-0 last:pb-0">
@@ -24,14 +22,6 @@ const Note: Component<Props> = ({ note }) => {
           >
             {title}
           </a>
-          <div>
-            <time
-              class="text-xs sm:text-sm text-gray-800 dark:text-gray"
-              datetime={publishDate.toISOString()}
-            >
-              {formattedDate(publishDate)}
-            </time>
-          </div>
         </div>
       </div>
     </li>
